56cd46c9d6cf913eb1cc5b03c786c519f5a43061
Christian Vivas <160914543+ChrisVivasAI@users.noreply.github.com>
2024-06-05 11:20:56 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,8 @@ export default authMiddleware({
   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
  
+
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
